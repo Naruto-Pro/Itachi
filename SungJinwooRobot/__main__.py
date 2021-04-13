730dab0b9fa2a77fe793336966d40c6cf3801270
Naruto-Pro <82357039+Naruto-Pro@users.noreply.github.com>
2021-04-13 03:39:42 +0000

    Update __main__.py

--- SungJinwooRobot/__main__.py
+++ SungJinwooRobot/__main__.py
@@ -58,7 +58,6 @@ I am an Anime themed group management bot. You can find my list of available com
 HELP_STRINGS = """
 Hey there! My name is ITACHI.
 I help admins manage their groups with my Power Level! 
-
 *Main* commands available:
  • /help: PM's you this message.
  • /help <module name>: PM's you info about that module.
@@ -66,8 +65,6 @@ I help admins manage their groups with my Power Level!
  • /settings:
    • in PM: will send you your settings for all supported modules.
    • in a group: will redirect you to pm, with all that chat's settings.
-
-
 {}
 And the following:
 """.format(
